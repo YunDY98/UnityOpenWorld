cb94c124f7c718567ef54574587868c05449cf04
yun <dbsejrdid123@gmail.com>
2024-04-22 23:03:57 +0900

    RaycastHit Gun

--- Assets/PlayerFire.cs
+++ Assets/PlayerFire.cs
@@ -10,6 +10,13 @@ public class PlayerFire : MonoBehaviour
 
     public float throwPower = 15f;
 
+    public GameObject bulletEffect;
+
+    public ParticleSystem ps;
+    void Start ()
+    {
+        //ps = bulletEffect.GetComponent<ParticleSystem>();
+    }
 
 
     // Update is called once per frame
@@ -24,6 +31,27 @@ public class PlayerFire : MonoBehaviour
             Rigidbody rb = bomb.GetComponent<Rigidbody>();
 
             rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            // 레이 시작점 과 방향 
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+
+            RaycastHit hitInfo = new RaycastHit();
+
+            if(Physics.Raycast(ray, out hitInfo)) // out키워드는 주소를 복사해 가져옮, 반드시 함수안에서 파라미터 값을 할당할 것을 요구 
+            {
+
+                // hit 지점에서 이팩트 
+                bulletEffect.transform.position = hitInfo.point;
+
+                // 이펙트 로워드 방향을 레이가 부딪힌 지점의 법선 벡터와 일치 시캄
+                bulletEffect.transform.forward = hitInfo.normal;
+
+                ps.Play();
+
+            }
         }
         
     }
