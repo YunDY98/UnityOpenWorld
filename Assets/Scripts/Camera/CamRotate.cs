96ce6bf229d65a9360b063c209ed26468a1793a6
yun <dbsejrdid123@gmail.com>
2025-04-03 20:37:59 +0900

    회전 Lerp값 조정

--- Assets/Scripts/Camera/CamRotate.cs
+++ Assets/Scripts/Camera/CamRotate.cs
@@ -9,7 +9,7 @@ public class CamRotate : MonoBehaviour
     float my = 0;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // 게임 중일때만 동작 
         if(GameManager.Instance.gState != GameManager.GameState.Run)
@@ -24,28 +24,22 @@ public class CamRotate : MonoBehaviour
             float mouse_X = Input.GetAxis("Mouse X");
             float mouse_Y = Input.GetAxis("Mouse Y");
 
-            //1-1. 회전 값 변수에 마우스 입력 값 만큼 미리 누적
-            // mx += mouse_X *GameManager.gameManager.rotSpeed * Time.deltaTime;
-            // my -= mouse_Y *GameManager.gameManager.rotSpeed * Time.deltaTime;
-
             // 스무딩을 적용하여 회전 값을 부드럽게 업데이트
-            mx = Mathf.Lerp(mx, mx + mouse_X * GameManager.Instance.rotSpeed * Time.deltaTime, 0.1f);
-            my = Mathf.Lerp(my, my - mouse_Y * GameManager.Instance.rotSpeed * Time.deltaTime, 0.1f);
+            mx = Mathf.Lerp(mx, mx + mouse_X * GameManager.Instance.rotSpeed, 0.01f * Time.deltaTime);
+            my = Mathf.Lerp(my, my - mouse_Y * GameManager.Instance.rotSpeed, 0.01f * Time.deltaTime);
 
     
             //1-2. 마우스 상하 이동 회전 변수(my)의 값을 -90 ~90 사이로 제한
             my = Mathf.Clamp(my,-89f,89f);
 
-            // // 부드러운 회전을 위한 보간
-            // currentX = Mathf.SmoothDamp(currentX, mx, ref currentXVelocity, smoothTime);
-            // currentY = Mathf.SmoothDamp(currentY, my, ref currentYVelocity, smoothTime);
-
-    
+           
             //2.마우스 입력 값을 이용해 회전 방향 결정
     
             transform.eulerAngles = new Vector3(my,mx,0);
 
         }
+
+        
        
        
     }
--- Assets/Scripts/Player/PlayerRotate.cs
+++ Assets/Scripts/Player/PlayerRotate.cs
@@ -28,7 +28,7 @@ public class PlayerRotate : MonoBehaviour
             //mx += mouse_X * GameManager.gameManager.rotSpeed * Time.deltaTime;
 
             // 스무딩을 적용하여 회전 값을 부드럽게 업데이트
-            mx = Mathf.Lerp(mx, mx + mouse_X * GameManager.Instance.rotSpeed * Time.deltaTime, 0.1f);
+            mx = Mathf.Lerp(mx, mx + mouse_X * GameManager.Instance.rotSpeed , 0.01f * Time.deltaTime);
             
 
             transform.eulerAngles = new Vector3(0,mx,0);
